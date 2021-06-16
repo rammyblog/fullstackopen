bd6fe3e0c8f0ca6d26f82ca8bc7825d5c2512667
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-06-16 20:35:17 +0100

    Done 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,77 @@
 import React, { useState } from "react";
 
+const Filter = ({ search, onChangeHandler }) => {
+  return (
+    <>
+      Filter shown with <input value={search} onChange={onChangeHandler} />
+    </>
+  );
+};
+
+const PersonForm = ({
+  newName,
+  handleNewName,
+  newNumber,
+  handleNewNumber,
+  handleFormSubmit,
+}) => {
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+const Person = ({ persons }) => {
+  return persons.map((person) => (
+    <>
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    </>
+  ));
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "01010100" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
+  const [personSearch, setPersonSearch] = useState([]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleNewName = (e) => {
     setNewName(e.target.value);
   };
   const handleNewNumber = (e) => {
     setNewNumber(e.target.value);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    if (e.target.value) {
+      setPersonSearch(
+        persons.filter((person) => person.name.toLowerCase().includes(search))
+      );
+    } else {
+      setPersonSearch([]);
+    }
   };
 
   const handleFormSubmit = (e) => {
@@ -24,24 +84,21 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>
-          number:{" "}
-          <input type="number" value={newNumber} onChange={handleNewNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter search={search} onChangeHandler={handleSearchChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        newName={newName}
+        handleNewName={handleNewName}
+        newNumber={newNumber}
+        handleNewNumber={handleNewNumber}
+        handleFormSubmit={handleFormSubmit}
+      />
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      {search ? (
+        <Person persons={personSearch} />
+      ) : (
+        <Person persons={persons} />
+      )}
     </div>
   );
 };
