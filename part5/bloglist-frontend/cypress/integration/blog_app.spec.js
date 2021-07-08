0ea94fa5e07019f79ebfe02b4ae2a3ee18242125
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-07-08 17:41:37 +0100

    5.18 done

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -6,9 +6,24 @@ describe("Blog app", function () {
 
   it("Login form is shown", function () {
     cy.contains("login");
-    cy.get("#username").type("rammy");
-    cy.get("#password").type("12345678");
-    cy.get("#login-btn").click();
-    cy.contains("Onasany Tunde logged in");
+    cy.get("#username");
+    cy.get("#password");
+    // cy.get("#login-btn").click();
+    // cy.contains("Onasany Tunde logged in");
+  });
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("rammy");
+      cy.get("#password").type("12345678");
+      cy.get("#login-btn").click();
+      cy.contains("Onasany Tunde logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("rammy");
+      cy.get("#password").type("12345");
+      cy.get("#login-btn").click();
+      cy.contains("wrong credentials");
+    });
   });
 });
